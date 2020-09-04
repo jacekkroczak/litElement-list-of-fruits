874aa3b7144a4fd862c193e5cb2480fbb890a99c
jacekkroczak <jacek.kroczak1995@gmail.com>
2020-09-04 11:46:59 +0200

    filters

--- src/jk-list.ts
+++ src/jk-list.ts
@@ -9,15 +9,86 @@ export class JkList extends LitElement {
   static get styles() {
     return [
       css`
+          #filters {
+            justify-content: space-between;
+            display: flex;
+            max-width: 65%;
+            padding: 20px 0;
+          }
           ul {
             column-count: 5;
             list-style: none;
+            padding-left: 0;
           }
+
+          button {
+            border: 2px solid;
+            padding: 0 30px;
+            height: 45px;
+            background: transparent;
+            font-size: 14px;
+            font-weight: bold;
+            text-transform: uppercase;
+            letter-spacing: 0.5px;
+            cursor: pointer;
+            transition: 0.5s;
+          }
+
+          button:focus {
+            outline: none;
+          }
+
+          .purpleButton {
+            border-color: #904ae6;
+            color: #904ae6;
+          }
+
+          .blueButton {
+            border-color: #35b8ee;
+            color: #35b8ee;
+          }
+
+          .greenButton {
+            border-color: #3fb984;
+            color: #3fb984;
+          }
+
+          .redButton {
+            border-color: #c3002f;
+            color: #c3002f;
+          }
+
+          .purpleButton:hover {
+            background: #904ae6;
+            color: #fff;
+            border-color: #904ae6;
+          }
+
+          .blueButton:hover {
+            background: #35b8ee;
+            color: #fff;
+            border-color: #35b8ee;
+          }
+
+          .greenButton:hover {
+            background: #3fb984;
+            color: #fff;
+            border-color: #3fb984;
+          }
+
+          .redButton:hover {
+            background: #c3002f;
+            color: #fff;
+            border-color: #c3002f;
+          }
+
       `,
     ];
   }
 
   @property({ type: Array}) fruits: any = [];
+
+  @property({ type: Array}) filteredFruits: any = [];
 
   constructor() {
     super();
@@ -25,13 +96,43 @@ export class JkList extends LitElement {
 
   firstUpdated() {
     this.fruits = []
-      for (let i = 0; i < 1000; i++) {
-        const index = Math.floor(Math.random() * array.length);
-        const itemFruit = array[index];
-        this.fruits.push(itemFruit);
-      }
 
-      console.log('array', this.fruits);
+    for (let i = 0; i < 100; i++) {
+      for (let i = 0; i < array.length; i++) {
+      let itemFruit = array[i];
+      this.fruits.push(itemFruit);
+      }
+    }
+    
+  }
+
+  _filterByAZ() {
+    this.fruits.sort((a: any, b: any) => {
+      if(a.name < b.name) { return -1; }
+      return 0;
+    })
+    this.requestUpdate();
+  }
+
+  _filterByZA() {
+    this.fruits.sort((a: any, b: any) => {
+      if(a.name > b.name) { return -1; }
+      return 0;
+    })
+    this.requestUpdate();
+  }
+
+  _filterFirstMango() {
+    this.fruits.sort((item: any) => {
+      if(item.name === 'Mango') { return -1; }
+      return 0;
+    })
+      this.requestUpdate();
+  }
+
+  _reset() {
+    this.firstUpdated();
+    this.requestUpdate();
   }
 
 
@@ -44,9 +145,17 @@ export class JkList extends LitElement {
     `;
 
     return html`
+      <div id="filters">
+          <button class="purpleButton" @click="${this._filterByAZ}">A-Z filter</button>
+          <button class="greenButton" @click="${this._filterByZA}">Z-A filter</button>
+          <button class="blueButton" @click="${this._filterFirstMango}">Show "Mango" in first column </button>
+          <button class="redButton" @click="${this._reset}">Reset</button>
+      </div>
+      <div id="list">
         <ul class="list">
           ${this.fruits.map((item: any, index: number) => _renderFruit(item, index))}
         </ul>
+      </div>
     `;
   }
 }
